f610db32456dd89931975495ba033d30f3e4c59d
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 08:26:18 +0200

    arithmetic in where clause

--- Retrieving data from a SINGLE table/where_clause.sql
+++ Retrieving data from a SINGLE table/where_clause.sql
@@ -27,3 +27,7 @@ WHERE NOT (birth_date > '1990-01-01' OR points > 1000);
 SELECT * 
 FROM customers
 WHERE birth_date <= '1990-01-01' AND points <= 1000;
+
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND unit_price * quantity > 30;
