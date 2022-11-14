3f8f896646ed2a05717402a97dd6bb6d92c0c71b
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 07:58:57 +0200

    arithmetic and alias

--- Retrieving data from a SINGLE table/select_statement.sql
+++ Retrieving data from a SINGLE table/select_statement.sql
@@ -1,6 +1,7 @@
 USE store;
 
-SELECT * 
-FROM customers
--- WHERE customer_id = 1
--- ORDER BY first_name;
+SELECT 
+	first_name, 
+    last_name,
+    (points + 10) * 100 AS 'discount_factor'
+FROM customers;
