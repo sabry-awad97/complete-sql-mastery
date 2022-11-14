b55fc3b33906803401a8d1cf2f45f9b10ee7107b
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 09:28:35 +0200

    limit operator

---
+++ Retrieving data from a SINGLE table/limit.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM sql_store.customers
+LIMIT 3;
+
+SELECT *
+FROM sql_store.customers
+ORDER BY points DESC
+LIMIT 3;
--- Retrieving data from a SINGLE table/order by.sql
+++ Retrieving data from a SINGLE table/order by.sql
@@ -1,20 +1,24 @@
 SELECT * 
-FROM sql_store.customers
+FROM customers
 ORDER BY first_name;
 
 SELECT * 
-FROM sql_store.customers
+FROM customers
 ORDER BY state DESC, first_name DESC;
 
 SELECT first_name, last_name
-FROM sql_store.customers
+FROM customers
 ORDER BY birth_date;
 
 SELECT first_name, last_name, 10 AS points
-FROM sql_store.customers
+FROM customers
 ORDER BY points, first_name;
 
+-- SELECT first_name, last_name, 10 AS points
+-- FROM customers
+-- ORDER BY 1, 2;
+
 SELECT *, quantity * unit_price AS total_price 
-FROM sql_store.order_items
+FROM order_items
 WHERE order_id = 2
 ORDER BY total_price DESC;
