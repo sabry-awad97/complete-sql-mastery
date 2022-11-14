228a1be5c83016b9e383c10c58c416c14309050f
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 12:59:07 +0200

    cross joins

---
+++ Retrieving data from MULTIPLE tables/cross_join.sql
@@ -0,0 +1,29 @@
+USE sql_store;
+
+SELECT 
+	c.first_name AS customer,
+    p.name AS product
+FROM customers c
+CROSS JOIN products p -- this is the explicit syntax for a cross join
+ORDER BY c.first_name;
+
+SELECT 
+	c.first_name AS customer,
+    p.name AS product
+FROM customers c, products p -- this is the equivalent to the explicit syntax's result
+ORDER BY c.first_name;
+
+-- implicit syntax of CROSS JOIN
+SELECT 
+	sh.name AS shippers,
+    p.name AS products
+FROM shippers sh, products p
+ORDER BY sh.name;
+
+-- explicit syntax of CROSS JOIN
+SELECT 
+	sh.name AS shippers,
+    p.name AS products
+FROM shippers sh
+CROSS JOIN products p
+ORDER BY sh.name;
