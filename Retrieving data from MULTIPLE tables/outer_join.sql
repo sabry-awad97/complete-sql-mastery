f03a59ebcde44760d4283a973bb33eb43d2c0880
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 12:05:32 +0200

    join multiple tables

--- Retrieving data from MULTIPLE tables/outer_join.sql
+++ Retrieving data from MULTIPLE tables/outer_join.sql
@@ -23,3 +23,29 @@ SELECT
 FROM products p
 LEFT JOIN order_items oi
 	ON p.product_id = oi.product_id;
+
+SELECT 
+	c.customer_id,
+    c.first_name,
+    o.order_id,
+    sh.name AS shipper
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON sh.shipper_id = o.shipper_id;
+    
+SELECT 
+	o.order_date,
+    o.order_id,
+    c.first_name AS customer,
+    sh.name AS shipper,
+    os.name AS status
+FROM orders o
+LEFT JOIN customers c
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers sh
+	ON sh.shipper_id = o.shipper_id
+LEFT JOIN order_statuses os
+	ON os.order_status_id = o.status
+ORDER BY status;
