69569c66561afdae175921c743d100e45c2c3d75
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-14 09:45:22 +0200

    inner join

---
+++ Retrieving data from MULTIPLE tables/inner_join.sql
@@ -0,0 +1,24 @@
+SELECT order_id, first_name, last_name
+FROM orders
+JOIN customers
+	ON orders.customer_id = customers.customer_id;
+    
+SELECT order_id, orders.customer_id, first_name, last_name
+FROM orders
+JOIN customers
+	ON orders.customer_id = customers.customer_id;
+    
+SELECT order_id, o.customer_id, first_name, last_name
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id;
+    
+SELECT order_id, o.customer_id, first_name, last_name
+FROM orders AS o
+JOIN customers AS c
+	ON o.customer_id = c.customer_id;
+
+SELECT o.order_id, o.product_id, p.name, o.quantity, o.unit_price
+FROM sql_store.order_items AS o
+JOIN sql_store.products AS p
+	ON o.product_id = p.product_id;
