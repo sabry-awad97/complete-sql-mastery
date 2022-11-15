5f61217acddb945bd5261a03931f700aefc3cd67
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 04:11:52 +0200

    create table as

---
+++ CRUD/copy_table.sql
@@ -0,0 +1,27 @@
+CREATE TABLE orders_archived AS
+SELECT * FROM orders;
+
+TRUNCATE TABLE orders_archived;
+
+INSERT INTO orders_archived
+SELECT *
+FROM orders
+WHERE order_date < '2019-01-01';
+
+USE sql_invoicing;
+DROP TABLE invoices_archived;
+
+CREATE TABLE invoices_archived AS
+SELECT 
+	i.invoice_id,
+    i.number,
+    c.name AS client,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.due_date,
+    i.payment_date
+FROM invoices i 
+JOIN clients c
+	USING (client_id)
+WHERE payment_date IS NOT NULL;
