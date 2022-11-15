24fe9fd7b4b55c27dfb8225e55f604f96a3101d6
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 07:09:11 +0200

    all keyword

---
+++ Complex Queries/all.sql
@@ -0,0 +1,18 @@
+USE sql_invoicing;
+
+SELECT *
+FROM invoices
+WHERE invoice_total > (
+        SELECT
+            MAX(invoice_total)
+        FROM invoices
+        WHERE client_id = 3
+    );
+
+SELECT *
+FROM invoices
+WHERE invoice_total > ALL (
+        SELECT invoice_total
+        FROM invoices
+        WHERE client_id = 3
+    );
