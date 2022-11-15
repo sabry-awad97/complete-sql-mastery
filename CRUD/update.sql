d57434d143eb65c6e8d4098e4791d6bcfbea1337
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 04:32:40 +0200

    select subquery

--- CRUD/update.sql
+++ CRUD/update.sql
@@ -28,8 +28,45 @@ SET
     payment_date = due_date
 WHERE client_id IN (3, 4);
 
-USE sql_store;
-
 UPDATE customers
 SET points = points + 50
 WHERE birth_date < '1990-01-01';
+
+UPDATE invoices
+SET
+    payment_total = invoice_total * 0.5,
+    payment_date = due_date
+WHERE client_id = (
+        SELECT client_id
+        FROM clients
+        WHERE name = 'Myworks'
+    );
+
+UPDATE invoices
+SET
+    payment_total = invoice_total * 0.5,
+    payment_date = due_date
+WHERE client_id IN (
+        SELECT client_id
+        FROM clients
+        WHERE
+            state IN ('CA', 'NY')
+    );
+
+UPDATE orders
+SET comments = 'Gold customer'
+WHERE customer_id IN (
+        SELECT
+            DISTINCT c.customer_id
+        FROM orders o
+            JOIN customers c USING (customer_id)
+        WHERE c.points > 3000
+    );
+
+UPDATE orders
+SET comments = 'Gold customer'
+WHERE customer_id IN (
+        SELECT customer_id
+        FROM customers
+        WHERE points > 3000
+    );
