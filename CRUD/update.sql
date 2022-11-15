d2aa5e15c22b9eb7f7694b746e9d78bffd3314f4
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 04:15:06 +0200

    update single row

---
+++ CRUD/update.sql
@@ -0,0 +1,17 @@
+UPDATE invoices
+SET
+    payment_total = 10,
+    payment_date = '2019-01-03'
+WHERE invoice_id = 1;
+
+UPDATE invoices
+SET
+    payment_total = DEFAULT,
+    payment_date = NULL
+WHERE invoice_id = 1;
+
+UPDATE invoices
+SET
+    payment_total = invoice_total * 0.5,
+    payment_date = due_date
+WHERE invoice_id = 3;
