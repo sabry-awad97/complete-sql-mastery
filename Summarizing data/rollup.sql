581a4e6dea9b1d889cfceccbd79065922d8fd076
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 06:44:45 +0200

    with rollup

---
+++ Summarizing data/rollup.sql
@@ -0,0 +1,23 @@
+SELECT
+    client_id,
+    SUM(invoice_total) AS total_sales
+FROM invoices
+GROUP BY client_id
+WITH ROLLUP;
+
+SELECT
+    state,
+    city,
+    SUM(invoice_total) AS total_sales
+FROM invoices i
+    JOIN clients c USING (client_id)
+GROUP BY state, city
+WITH ROLLUP;
+
+SELECT
+    pm.name AS payment_method,
+    SUM(p.amount) AS total
+FROM payments p
+    JOIN payment_methods pm ON p.payment_method = pm.payment_method_id
+GROUP BY pm.name
+WITH ROLLUP;
