43df32e5ac8a73c52a40a47ce077404decb9584d
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-15 07:27:39 +0200

    exists operator

---
+++ Complex Queries/exists.sql
@@ -0,0 +1,29 @@
+SELECT *
+FROM clients
+WHERE client_id IN (
+        SELECT
+            DISTINCT client_id
+        FROM invoices
+    );
+
+SELECT *
+FROM clients c
+WHERE EXISTS (
+        SELECT client_id
+        FROM invoices
+        WHERE
+            client_id = c.client_id
+    );
+
+SELECT *
+FROM products p
+WHERE NOT EXISTS (
+        SELECT product_id
+        FROM order_items
+        WHERE
+            product_id = p.product_id
+    );
+
+-- when a subquery returns a lot of data, it
+
+-- is more efficient to use EXISTS operator instead.
