5b615dfeda93ef03b1883ef28ed8809c4e2a4606
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-16 07:32:38 +0200

    create index

---
+++ Indexes/create.sql
@@ -0,0 +1,7 @@
+EXPLAIN SELECT customer_id FROM customers WHERE state = 'CA';
+
+CREATE INDEX idx_state ON customers (state);
+
+EXPLAIN SELECT customer_id FROM customers WHERE points > 1000;
+
+CREATE INDEX idx_points ON customers (points);
