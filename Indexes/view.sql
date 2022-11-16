62bb5f2830e0a03574461a0b36f719059a02e2fc
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-16 07:32:47 +0200

    view indexes

---
+++ Indexes/view.sql
@@ -0,0 +1,10 @@
+SHOW INDEXES IN customers;
+
+ANALYZE TABLE customers;
+
+SHOW INDEXES IN orders;
+
+/* 
+ The PRIMARY key index is also called a CLUSTERED index, which is
+ automatically created when a primary key is set. 
+ */
