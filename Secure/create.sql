4c08e9d0866af77a9a0ae9713de805b3aea2b608
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-16 08:50:12 +0200

    create user

---
+++ Secure/create.sql
@@ -0,0 +1,10 @@
+CREATE USER
+    biljana @127.0.0.1 -- with IP
+CREATE USER
+    biljana @localhost -- with host
+CREATE USER
+    biljana @biljana - zobenica.github.io -- with domain
+CREATE USER
+    biljana @'%.biljana-zobenica.github.io' -- with any subdomains
+CREATE USER
+    biljana IDENTIFIED BY '1234';
