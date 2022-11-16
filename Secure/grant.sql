97e5bcec2364ff3df6167f4ade277cb958733a8a
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-16 09:04:09 +0200

    grant privileges

---
+++ Secure/grant.sql
@@ -0,0 +1,20 @@
+-- Granting privileges
+
+-- 1: web/desktop application
+
+CREATE USER bika_app IDENTIFIED BY '1234';
+
+GRANT
+SELECT,
+INSERT,
+UPDATE,
+DELETE,
+EXECUTE
+    ON sql_store.* -- or specify a table name instead of all by *;
+    TO bika_app;
+
+-- 2: admin
+
+GRANT
+    ALL ON *.* -- (all tables in all databases)
+    TO biljana;
